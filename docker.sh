e3d0c1cec05d660f05c8243f592ddf19b6b5dfdb
Huỳnh Văn Vọng <94370927+VongHuynh@users.noreply.github.com>
2023-05-18 21:48:05 +0700

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,13 @@
+docker stop monitoring-app | true
+docker rm monitoring-app | true
+
+until docker build -t monitoring-app .
+
+do
+  echo "Waiting build image..."
+  sleep 2
+done
+
+docker run --rm -it -p 5000:5000 --name monitoring monitoring
+
+echo "Run sucess"
